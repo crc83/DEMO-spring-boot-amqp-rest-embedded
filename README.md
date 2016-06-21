96d1101d4b4fcfbb1ebaca779e7a0fe8f9a4192d
Sergiy Beley <Sergiy.Beley@gmail.com>
2016-06-21 06:57:36 +0300

    initial commit (project generated with start.spring.io)

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # spring-boot-amqp-rest-embedded
+This is sandbox project just to verify what is necessary to make rest service with embedded messaging bus wrapped up with spring boot.
---
+++ src/main/java/org/sbelei/DemoamqpApplication.java
@@ -0,0 +1,12 @@
+package org.sbelei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoamqpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoamqpApplication.class, args);
+	}
+}
