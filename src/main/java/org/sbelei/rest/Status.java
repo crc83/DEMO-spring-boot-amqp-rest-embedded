8702d096d20027f6708f77651362bffcf4767f2c
Sergiy Beley <Sergiy.Beley@gmail.com>
2016-06-21 09:32:10 +0300

    we need setters/getters to make serializer working well

--- src/main/java/org/sbelei/rest/Status.java
+++ src/main/java/org/sbelei/rest/Status.java
@@ -7,4 +7,12 @@ public class Status {
 	public Status(String status) {
 		this.status = status;
 	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
 }
